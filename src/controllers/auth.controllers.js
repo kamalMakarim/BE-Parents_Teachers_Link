7e8fbea5123928f962e1d4e7bf07c64cecf64d5e
kamalMakarim <kamal.makarim@gmail.com>
2024-07-03 19:24:06 +0700

    auth fixes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,9 +3,14 @@ const authServices = require('../services/auth.service');
 exports.login = async (req, res) => {
     try {
         const response = await authServices.login(req.body);
-        res.cookie('token', response.token, { httpOnly: true, maxAge: 3600000 });    
+        res.cookie('token', response.token, { 
+            httpOnly: true, 
+            maxAge: 3600000, // 1 hour in milliseconds
+            secure: true,    // Ensures the cookie is sent over HTTPS
+            sameSite: 'strict' // Adjust according to your needs, could also be 'lax' or 'none' (if cross-site)
+        });
         res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const userControllers = require('../controllers/user.controllers');
 const router = require('express').Router();
 const auth = require('../middlewares/auth.middlewares');
-router.post('/add',userControllers.addUser);
+router.post('/add',auth.authenticate, auth.isAdmin,userControllers.addUser);
 router.put('/update-password', auth.authenticate, userControllers.updatePasswordByUser);
 router.put('/update-password-admin', auth.authenticate, auth.isAdmin, userControllers.updatePasswordByAdmin);
 router.put('/update-display-name', auth.authenticate, userControllers.updateDisplayName);   
