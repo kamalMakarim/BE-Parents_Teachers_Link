a66d1e3b5f83d5c4e84780f2f40b4022852ed6dd
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 01:58:16 +0700

    same site none

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -6,7 +6,7 @@ exports.login = async (req, res) => {
         res.cookie('token', response.token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
         console.log(response);
