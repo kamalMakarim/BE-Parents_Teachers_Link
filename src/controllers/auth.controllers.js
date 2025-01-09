0f74a50c4937eda8f81af64e5742c6216b412429
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 00:34:26 +0700

    same site none

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -6,6 +6,7 @@ exports.login = async (req, res) => {
         res.cookie('token', response.token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',   
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
         res.status(200).json(response);
