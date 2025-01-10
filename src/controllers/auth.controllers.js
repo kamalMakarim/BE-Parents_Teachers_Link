b0eca050ad6297349644acb6a86c1d430b6f286e
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 08:59:11 +0700

    added domain for cookie

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -7,6 +7,7 @@ exports.login = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
+            domain: '.sekolahbinekas.com',
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
         console.log(response);
