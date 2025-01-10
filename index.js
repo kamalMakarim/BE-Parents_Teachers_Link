83e93b35c965f3255a8f504d0c39ce721e71e618
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 10:40:57 +0700

    back to fe url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 const corsOptions = {
-  origin: "*",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -25,7 +25,6 @@ app.use((req, res, next) => {
 });
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 require("./src/config/monggo.config").connectDB();
 app.use(express.json());
