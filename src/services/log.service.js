82c972710ce8aa3be6abbda4347b64f36c1359e7
kamalMakarim <kamal.makarim@gmail.com>
2025-02-14 22:34:25 +0700

    2 months

--- src/services/log.service.js
+++ src/services/log.service.js
@@ -194,7 +194,7 @@ exports.getLogOfStudent = async function (req) {
 
     // Calculate the time limit (1 day before the given timestamp)
     const timeLimit = new Date(timestamp);
-    timeLimit.setDate(timeLimit.getDate() - 45);
+    timeLimit.setDate(timeLimit.getDate() - 60);
 
     const bidangStudi = [
       "Blue Pinter Morning",
