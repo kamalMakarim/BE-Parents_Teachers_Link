7bf1f55f811a7dfdd03138e10da1912937b1b920
kamalMakarim <kamal.makarim@gmail.com>
2025-03-03 20:11:38 +0700

    change to 3 days

--- src/services/log.service.js
+++ src/services/log.service.js
@@ -194,7 +194,7 @@ exports.getLogOfStudent = async function (req) {
 
     // Calculate the time limit (1 day before the given timestamp)
     const timeLimit = new Date(timestamp);
-    timeLimit.setDate(timeLimit.getDate() - 10);
+    timeLimit.setDate(timeLimit.getDate() - 3);
 
     const bidangStudi = [
       "Blue Pinter Morning",
